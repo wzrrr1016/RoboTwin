f7d752724c411ec211185a50d6b7ffde6a86fb2d
wzrrr1016 <2425885380@qq.com>
2025-10-22 13:29:02 +0800

    add collect_data_save_all.sh

---
+++ collect_data_save_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# bash collect_data.sh <task_name> <task_config> <gpu_id>
+# e.g. bash collect_data.sh pick_place_block_2r2b demo_randomized 0
+
+task_name=${1}
+task_config=${2}
+gpu_id=${3}
+
+./script/.update_path.sh > /dev/null 2>&1
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_VISIBLE_DEVICES=${gpu_id}
+
+PYTHONWARNINGS=ignore::UserWarning \
+python script/collect_data_save_all.py $task_name $task_config
+rm -rf data/${task_name}/${task_config}/.cache
